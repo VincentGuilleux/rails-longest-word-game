630ac8b0143d18a9988c19d0ea29e012f203d6dd
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-03-30 17:06:35 +0200

    all finished including upgrading score

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,6 +11,7 @@ class GamesController < ApplicationController
     english_word_check = JSON.parse(english_word_dict)
     word_array = params[:word].upcase.chars
     word_in_grid = word_array.all? { |e| params[:random_word].include?(e) }
+    # non correct car ne checke pas des lettres repetees, par ex. TEST sur TESAAAAAAA fonctionne alors que devrait pas
     if english_word_check['found']
       # the key found returns a boolean if included in English in Wagon dic app
       word_in_grid ? session[:score] = session[:score] + word_array.length : @result = 'outside the grid'
