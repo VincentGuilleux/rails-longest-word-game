c27916a5c5105da4f373cd839c2504a7d9d77907
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-03-30 16:15:17 +0200

    Longest Word game ok

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,21 +2,18 @@ require 'open-uri'
 
 class GamesController < ApplicationController
   def new
-    @letters = Array.new(10) { Array('A'..'Z').sample }
+    @random_word = Array.new(10) { Array('A'..'Z').sample }
   end
 
   def score
     url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
     english_word_dict = open(url).read
     english_word_check = JSON.parse(english_word_dict)
-    @letters
     word_array = params[:word].upcase.chars
-    # check = word_array.all? { |e| @letters.include?(e) }
-    raise
+    word_in_grid = word_array.all? { |e| params[:random_word].include?(e) }
     if english_word_check['found']
       # the key found returns a boolean if included in English in Wagon dic app
-      @result = 'ok english word'
-
+      @result = 'outside the grid' unless word_in_grid
     else @result = 'not an english word'
     end
   end
