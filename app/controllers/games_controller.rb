5e2fbae53ae4019391b5899fb02cd78e86e78c00
Vincent Guilleux <vincent.guilleux@yahoo.fr>
2020-03-30 15:04:17 +0200

    Checking logic in progress

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,17 @@
-class GamesController < ApplicationController
+require 'open-uri'
 
+class GamesController < ApplicationController
   def new
+    @letters = Array.new(10) { Array('A'..'Z').sample }
   end
 
   def score
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    english_word_dict = open(url).read
+    english_word_check = JSON.parse(english_word_dict)
+    if english_word_check["found"]
+      @result = "ok english word"
+    else @result = "not an english word"
+    end
   end
-
 end
